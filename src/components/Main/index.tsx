9be02cbec9345d23724ac89a18ba5b5b3d86e164
Renan Souza <renantrsouza@gmail.com>
2022-08-19 11:56:07 -0400

    add: imports and components

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,5 +1,14 @@
-import style from './Main.module.scss'
+import s from './Main.module.scss'
+import { Home } from '../Home'
+import { Sponsors } from '../Sponsors'
+import { Specifications } from '../Specifications'
 
 export const Main = () => {
-  return <main className={style.main}></main>
+  return (
+    <main className={s.main}>
+      <Home />
+      <Sponsors />
+      <Specifications />
+    </main>
+  )
 }
