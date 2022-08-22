dc0a59cbf1801e9fb34d934a0ba808af8d135d41
Renan Souza <renantrsouza@gmail.com>
2022-08-22 11:48:07 -0400

    feat: create case component

---
+++ src/components/Case/index.tsx
@@ -0,0 +1,49 @@
+import s from './Case.module.scss'
+
+import caseImg from '../../assets/img/case.png'
+
+import { Button } from '../Button'
+import { Info } from 'phosphor-react'
+import { Section } from '../Section'
+import { Container } from '../Container'
+
+export const Case = () => {
+  return (
+    <Section>
+      <h2 className="section__title section__title-gradient">Case</h2>
+
+      <Container containerClass={s.case__container}>
+        <div>
+          <img src={caseImg} className={s.case__img} alt="" />
+        </div>
+
+        <div className={s.case__data}>
+          <p className={s.case__description}>
+            With a comfortable and adaptable case so that you can store it
+            whenever you want, and keep your durabillity forever.
+          </p>
+
+          <Button>
+            <Info />
+            More info
+          </Button>
+        </div>
+      </Container>
+
+      {/* <div className={`${s.case__container} container grid`}>
+        <div>
+          <img src={caseImg} className={s.case__img} alt="" />
+        </div>
+
+        <div className={s.case__data}>
+          <p className={s.case__description}></p>
+
+          <Button>
+            <Info />
+            More info
+          </Button>
+        </div>
+      </div> */}
+    </Section>
+  )
+}
