a54ebd101ab1c3a6ee9239d97289e07b2f7227f3
Renan Souza <renantrsouza@gmail.com>
2022-08-24 11:41:29 -0400

    update: create a normal button for button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,24 @@
 import { ReactNode } from 'react'
 
 interface ButtonProps {
+  styleButton?: any
   children: ReactNode
 }
 
-export const Button = ({ children }: ButtonProps) => {
+const LinkButton = ({ children }: ButtonProps) => {
   return (
     <a href="#" className={`button button__flex`}>
       {children}
     </a>
   )
 }
+
+const NormalButton = ({ styleButton, children }: ButtonProps) => {
+  return (
+    <button className={`${styleButton ? styleButton : 'button'} button__flex`}>
+      {children}
+    </button>
+  )
+}
+
+export const Button = { LinkButton, NormalButton }
