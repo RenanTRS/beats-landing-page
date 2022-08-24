e1b7dc83f4961a4c8fad032701eac2bba9430978
Renan Souza <renantrsouza@gmail.com>
2022-08-24 11:46:43 -0400

    update: add new atributes

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,9 +1,18 @@
 import { ReactNode } from 'react'
 
 interface TitleProps {
+  titleStyle?: any
   children: ReactNode
 }
 
-export const Title = ({ children }: TitleProps) => {
-  return <h2 className="section__title section__title-gradient">{children}</h2>
+export const Title = ({ titleStyle, children }: TitleProps) => {
+  return (
+    <h2
+      className={`section__title section__title-gradient ${
+        titleStyle ? titleStyle : ''
+      } `}
+    >
+      {children}
+    </h2>
+  )
 }
