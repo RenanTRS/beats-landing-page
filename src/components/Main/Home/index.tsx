8e54d08c82122b16a9e09734415fdfa5f3750054
Renan Souza <renantrsouza@gmail.com>
2022-08-31 15:09:52 -0400

    style: add reveal in the section title and home component

--- src/components/Main/Home/index.tsx
+++ src/components/Main/Home/index.tsx
@@ -1,6 +1,8 @@
 import homeImg from '../../../assets/img/home.png'
 import s from './Home.module.scss'
 
+import { motion } from 'framer-motion'
+
 import { Handbag } from 'phosphor-react'
 import { Button } from '../../Button'
 import { Container } from '../../Container'
@@ -9,18 +11,32 @@ export const Home = () => {
   return (
     <section className={`section`} id="home">
       <Container containerClass={s.home__container}>
-        <div>
+        <motion.div
+          initial={{ y: -100, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 2, delay: 0.9 }}
+        >
           <img src={homeImg} alt="" className={s.home__img} />
-        </div>
+        </motion.div>
 
         <div className={s.home__data}>
-          <div className={s.home__header}>
+          <motion.div
+            className={s.home__header}
+            initial={{ y: 60, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 2, delay: 0.4 }}
+          >
             <h1 className={s.home__title}>On ear</h1>
 
             <h2 className={s.home__subtitle}>Beats 3</h2>
-          </div>
+          </motion.div>
 
-          <div className={s.home__footer}>
+          <motion.div
+            className={s.home__footer}
+            initial={{ y: 60, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 2, delay: 0.7 }}
+          >
             <h3 className={s.home__title_description}>Overview</h3>
 
             <p className={s.home__description}>
@@ -36,7 +52,7 @@ export const Home = () => {
 
               <span className={s.home__price}>$299</span>
             </Button.LinkButton>
-          </div>
+          </motion.div>
         </div>
       </Container>
     </section>
